7bb475b87489d32a6f23c8e979f61e98a0c0b592
Brokenwind <wangkun6536@163.com>
2019-03-10 10:47:39 +0800

    change: 修改处理后的文件为csv

--- shell/extract_logs.sh
+++ shell/extract_logs.sh
@@ -8,7 +8,7 @@ if [ -d "$file_path" ]; then
     cd "$file_path"
     for file in `ls ?*.log`
     do
-        output_file="$file"_process.txt
-        cat $file  | grep -E "^ -" | awk -F '[-:]' 'BEGIN {print "时间\t\t损失值\t交叉熵\t训练精度\t验证损失\t验证交叉熵\t验证精度"} {print $2,$4,$6,$8,$10,$12,$14}' > $output_file
+        output_file="$file"_process.csv
+        cat $file  | grep -E "^ -" | awk -F '[-:]' 'BEGIN {print " 时间 , 损失值 , 交叉熵 , 训练精度 ,验证损失,验证交叉熵,验证精度"} {printf("%s,%s,%s,%s,%s,%s,%s\n",$2,$4,$6,$8,$10,$12,$14)}' > $output_file
     done
 fi

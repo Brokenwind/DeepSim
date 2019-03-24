a9449da1a9eb9c1b5363607b687bba86af7a106c
Brokenwind <wangkun6536@163.com>
2019-03-24 11:24:16 +0800

    add: Added shell to download neccessary packages

---
+++ shell/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo yum install opencc
+
+pip3 install numpy
+pip3 install pandas
+pip3 install matplotlib
+pip3 install ipython
+pip3 install seaborn
+pip3 install tensorflow-gpu
+pip3 install keras
+pip3 install gensim
+pip3 install opencc
+pip3 install Cython
+pip3 install fasttext
+pip3 install sklearn
+pip3 install scipy
+pip3 install jieba_fast
